1b468e143360410fa8e7b29e063e67a7a4506c22
Prince Jan Kumi <janprince002@gmail.com>
2022-04-25 15:05:45 +0000

    Add flights view

--- flight/urls.py
+++ flight/urls.py
@@ -3,7 +3,7 @@ from rest_framework.authtoken import views as auth_views
 from . import views
 
 urlpatterns = [
-
+    path("flights/", views.flights, name='flights'),
 
     # api endpoint for obtaining authentication token for a user
     path("api-token-auth/", auth_views.obtain_auth_token),
--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from .serializers import *
+from django.http import JsonResponse
+
 
 # Create your views here.
+def flights(request):
+    all_flights = Flight.object.all()
+    flights_serializer = FlightSerializer(all_flights, many=True)
+    return JsonResponse(flights_serializer, safe=False)
+
+
+
