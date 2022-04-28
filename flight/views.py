4b3b0881125eb4363b5cac3868096160141a930f
Prince Jan Kumi <janprince002@gmail.com>
2022-04-28 14:23:18 +0000

    Add permission decorator to flights view; Add file to test authentication

--- flight/views.py
+++ flight/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .models import *
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.csrf import csrf_exempt
 from .serializers import *
 from django.http import JsonResponse
 
 
 # Create your views here.
+@api_view(['GET',])
+@permission_classes((IsAuthenticated, ))
 def flights(request):
     all_flights = Flight.objects.all()
     flights_serializer = FlightSerializer(all_flights, many=True)
